b1cdca1aa346eceaba8436ff328b1e322ec3f96d
Dmitry <fendermany@gmail.com>
2021-03-18 06:28:55 +0200

    full

---
+++ dist/js/script.js
@@ -0,0 +1,104 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.promo__menu-list'),
+    menuItem = document.querySelectorAll('.promo__menu-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('promo__menu-list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('promo__menu-list_active');
+        });
+    });
+});
+
+
+$('[data-fancybox="gallery"]').fancybox({
+	thumbs : {
+        arrows: true,
+        loop: true,
+	}
+});
+
+
+jQuery("document").ready(function($){
+ 
+	var nav = $('.promo__header');
+ 
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 625) {
+			nav.addClass("f-nav");
+		} else {
+			nav.removeClass("f-nav");
+		}
+	});
+});
+
+// Проверяем, можно ли использовать Webp формат
+function canUseWebp() {
+    // Создаем элемент canvas
+    let elem = document.createElement('canvas');
+    // Приводим элемент к булеву типу
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // Создаем изображение в формате webp, возвращаем индекс искомого элемента и сразу же проверяем его
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+    // Иначе Webp не используем
+    return false;
+  }
+  
+  window.onload = function () {
+    // Получаем все элементы с дата-атрибутом data-bg
+    let images = document.querySelectorAll('[data-bg]');
+    // Проходимся по каждому
+    for (let i = 0; i < images.length; i++) {
+        // Получаем значение каждого дата-атрибута
+        let image = images[i].getAttribute('data-bg');
+        // Каждому найденному элементу задаем свойство background-image с изображение формата jpg
+        images[i].style.backgroundImage = 'url(' + image + ')';
+    }
+  
+    // Проверяем, является ли браузер посетителя сайта Firefox и получаем его версию
+    let isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+    let firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
+  
+    // Если есть поддержка Webp или браузер Firefox версии больше или равно 65
+    if (canUseWebp() || firefoxVer >= 65) {
+        // Делаем все то же самое что и для jpg, но уже для изображений формата Webp
+        let imagesWebp = document.querySelectorAll('[data-bg-webp]');
+        for (let i = 0; i < imagesWebp.length; i++) {
+            let imageWebp = imagesWebp[i].getAttribute('data-bg-webp');
+            imagesWebp[i].style.backgroundImage = 'url(' + imageWebp + ')';
+        }
+    }
+  };
+
+
+
+  var linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+    V = 1;  // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+for (var i = 0; i < linkNav.length; i++) {
+    linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+        e.preventDefault(); //отменяем стандартное поведение
+        var w = window.pageYOffset,  // производим прокрутка прокрутка
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  // к id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+            start = null;
+        requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+        function step(time) {
+            if (start === null) start = time;
+            var progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  // URL с хэшем
+            }
+        }
+    }, false);
+}
